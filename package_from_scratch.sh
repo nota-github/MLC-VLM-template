d52ec08fe91cf05b9e3d0dda31171023071e019b
Jaeyeon Kim <0310kjy@gmail.com>
2024-07-10 17:00:25 +0900

    modify package script

--- package_from_scratch.sh
+++ package_from_scratch.sh
@@ -1,11 +1,11 @@
 set -e
 
-MODEL_NAME=llava-1.5-7b-hf
+MODEL_NAME=phiva-4b-hf
 QUANTIZATION=q4f16_1
 
 # 1. Download weights from HF
 git lfs install
-git clone https://huggingface.co/llava-hf/$MODEL_NAME \
+git clone https://huggingface.co/nota-ai/$MODEL_NAME \
           ./dist/models/$MODEL_NAME
           
 # 2. convert weights
@@ -13,13 +13,13 @@ mlc_llm convert_weight ./dist/models/$MODEL_NAME/ --quantization $QUANTIZATION -
 
 # 3. create mlc-chat-config.json
 mlc_llm gen_config ./dist/models/$MODEL_NAME/ --quantization $QUANTIZATION \
-  --conv-template llava --context-window-size 768 -o dist/${MODEL_NAME}-${QUANTIZATION}-MLC/
+  --conv-template phiva --context-window-size 768 -o dist/${MODEL_NAME}-${QUANTIZATION}-MLC/
 
 # 4. compile model library with specification in mlc-chat-config.json
 mlc_llm compile ./dist/${MODEL_NAME}-${QUANTIZATION}-MLC/mlc-chat-config.json \
     --device android -o ./dist/${MODEL_NAME}-${QUANTIZATION}-MLC/${MODEL_NAME}-${QUANTIZATION}-android.tar
 
-# 5. Generate model execution logics
+# # 5. Generate model execution logics
 cd android/library
 chmod +x prepare_libs.sh
 bash prepare_libs.sh
