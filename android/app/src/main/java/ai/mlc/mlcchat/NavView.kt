353c834fa6aa4f2fde5ab19c4918c950ba374fc2
Jaeyeon Kim <0310kjy@gmail.com>
2024-07-16 17:43:10 +0900

    load json file in NavView

--- android/app/src/main/java/ai/mlc/mlcchat/NavView.kt
+++ android/app/src/main/java/ai/mlc/mlcchat/NavView.kt
@@ -7,13 +7,17 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import org.json.JSONObject
 
 @ExperimentalMaterial3Api
 @Composable
 fun NavView(activity: Activity, appViewModel: AppViewModel = viewModel()) {
     val navController = rememberNavController()
+    val json = (activity as MainActivity).assets.open("inputs.json").reader().readText()
+    val data = JSONObject(json).getJSONArray("data")
+
     NavHost(navController = navController, startDestination = "home") {
         composable("home") { StartView(navController, appViewModel) }
-        composable("chat") { ChatView(navController, appViewModel.chatState, activity) }
+        composable("chat") { ChatView(navController, appViewModel.chatState, activity, data) }
     }
 }
